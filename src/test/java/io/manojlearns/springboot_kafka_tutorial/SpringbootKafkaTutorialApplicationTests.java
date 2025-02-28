eed189278ac628f7346a5c044da9df29796a29b6
Sai Manoj Karanam <151407816+manojkaranamm-stack@users.noreply.github.com>
2025-02-28 15:36:33 -0500

    adding kafka properties in application.properties file

---
+++ src/test/java/io/manojlearns/springboot_kafka_tutorial/SpringbootKafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package io.manojlearns.springboot_kafka_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
